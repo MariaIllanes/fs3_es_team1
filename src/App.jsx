63cfa002bf057a16724d7341afcebd39d61e3b43
Sarkix <142878605+Sarkix@users.noreply.github.com>
2023-11-30 16:21:03 +0100

    feat: first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ArrowButton from './components/arrow-button.jsx'
+
+function App() {
+  
+  
+  return (
+    <>
+      <section id="other-element-container" className="other-element-container">
+      <ArrowButton/>
+
+      </section >
+    </>
+  )
+}
+
+export default App
---
+++ src/components/arrow-button.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect } from 'react';
+
+const ArrowButton = () => {
+    const svgStyle = {
+        marginLeft: "0px",
+    };
+    
+    useEffect(() => {
+        const arrowButton = document.querySelector(".other-element-button");
+        const buttonContainer = document.querySelector(".other-element-container");
+        const scrollThreshold = 0.25;
+
+        // Función para llevar al usuario al inicio de la página
+        const handleButtonClick = () => {
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+            });
+
+            arrowButton.style.transform = "scale(0.95)";
+
+            setTimeout(() => {
+                arrowButton.style.transform = "scale(1)";
+            }, 200);
+        };
+
+        arrowButton.addEventListener("click", handleButtonClick);
+
+        // Función para controlar la visibilidad de la sección en función del desplazamiento
+        const toggleSectionVisibility = () => {
+            const scrollPosition = window.scrollY;
+            const pageHeight = document.body.clientHeight;
+
+            if (scrollPosition >= pageHeight * scrollThreshold) {
+                buttonContainer.style.display = "block";
+            } else {
+                buttonContainer.style.display = "none";
+            }
+        };
+
+        window.addEventListener("scroll", toggleSectionVisibility);
+
+        // Asegúrate de verificar la visibilidad de la sección al cargar la página
+        toggleSectionVisibility();
+
+        // Limpia el evento al desmontar el componente
+        return () => {
+            arrowButton.removeEventListener("click", handleButtonClick);
+            window.removeEventListener("scroll", toggleSectionVisibility);
+        };
+    }, []); // El array vacío asegura que este efecto se ejecute solo una vez al montar el componente.
+
+    return (
+       
+        <div className="arrow-div-container">
+            <button className="other-element-button light-theme theme-change" width="48" height="48" variant="primary" scale="md">
+                <svg viewBox="0 0 24 24" color="invertedContrast" ml="0.5rem" width="20px" xmlns="http://www.w3.org/2000/svg" style={svgStyle}>
+                    <path d="M13 19V7.83001L17.88 12.71C18.27 13.1 18.91 13.1 19.3 12.71C19.69 12.32 19.69 11.69 19.3 11.3L12.71 4.71001C12.32 4.32001 11.69 4.32001 11.3 4.71001L4.69997 11.29C4.30997 11.68 4.30997 12.31 4.69997 12.7C5.08997 13.09 5.71997 13.09 6.10997 12.7L11 7.83001V19C11 19.55 11.45 20 12 20C12.55 20 13 19.55 13 19Z"></path>
+                </svg>
+            </button>
+        </div>
+    );
+};
+
+export default ArrowButton;
