9b098059a61cd840b6bfe64690b627f572cc607d
Sarkix <142878605+Sarkix@users.noreply.github.com>
2023-12-10 19:11:44 +0100

    Introduce ExchangeStatsOne component in React.
    Created the ExchangeStatsOne.jsx component to migrate the corresponding section from plain JavaScript to React. Additionally, added and imported the exchange-stats-one.css file for styling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import ArrowButton from './components/pages/home/arrowbutton/arrow-button.jsx'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import ArrowButton from "./components/pages/home/arrowbutton/arrow-button.jsx";
+import ExchangeStatsOne from "./components/pages/home/Exchange-stats-1/ExchangeStatsOne.jsx";
 
 function App() {
-  
-  
-  return (
-    <>
-      <section id="other-element-container" className="other-element-container">
-      <ArrowButton/>
-
-      </section >
-    </>
-  )
+    return (
+        <>
+            <section
+                id="other-element-container"
+                className="other-element-container"
+            >
+                <ArrowButton />
+            </section>
+            <section id="sec4-container" className="sec4-container">
+                <ExchangeStatsOne />
+            </section>
+        </>
+    );
 }
 
-export default App
+export default App;
