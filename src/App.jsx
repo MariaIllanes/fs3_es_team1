23db910ed9e97da0559157eb5f178017e27ff6a4
Sarkix <142878605+Sarkix@users.noreply.github.com>
2023-12-10 13:03:37 +0100

    Integrate News Component into React.
    Created the News.jsx component and imported it into App.jsx to migrate the news section from plain JavaScript to React. Additionally, added the news.css file and imported it into News.jsx for styling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import ArrowButton from './components/pages/home/arrowbutton/arrow-button.jsx'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import ArrowButton from "./components/pages/home/arrowbutton/arrow-button.jsx";
+import News from "./components/pages/home/News/News.jsx";
 
 function App() {
-  
-  
-  return (
-    <>
-      <section id="other-element-container" className="other-element-container">
-      <ArrowButton/>
-
-      </section >
-    </>
-  )
+    return (
+        <>
+            <section
+                id="other-element-container"
+                className="other-element-container"
+            >
+                <ArrowButton />
+            </section>
+            <section id="news">
+                <News />
+            </section>
+        </>
+    );
 }
 
-export default App
+export default App;
