097dfdcb57f90d86cbc7d93a4754f9cfbd0d8746
Sarkix <142878605+Sarkix@users.noreply.github.com>
2023-12-10 20:43:34 +0100

    Implement PartnersAnimation component in React.
    Created the PartnersAnimation.jsx component to transition the respective section from plain JavaScript to React. Also, added and imported the partners-animation.css file for styling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import ArrowButton from './components/pages/home/arrowbutton/arrow-button.jsx'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import ArrowButton from "./components/pages/home/arrowbutton/arrow-button.jsx";
+import PartnersAnimation from "./components/pages/home/partners-animation/PartnersAnimation.jsx";
 
 function App() {
-  
-  
-  return (
-    <>
-      <section id="other-element-container" className="other-element-container">
-      <ArrowButton/>
-
-      </section >
-    </>
-  )
+    return (
+        <>
+            <section
+                id="other-element-container"
+                className="other-element-container"
+            >
+                <ArrowButton />
+            </section>
+            <section id="partners-animation" style={{ zIndex: 5 }}>
+                <PartnersAnimation />
+            </section>
+        </>
+    );
 }
 
-export default App
+export default App;
