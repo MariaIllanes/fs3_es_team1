41f297df6b947218c5f30d29d97ea721145958d6
Daniel Martinez <104369426+dmg310802@users.noreply.github.com>
2023-12-11 11:08:18 -0600

    S1/main nav (#2)
    * feat: section 12 footer react started
    
    * feat: missing secondary footer + css
    
    * feact: missing dark-mode button
    
    * feat:missing dark button  s12 css corrected. ready to merge
    
    * css fixed
    
    * feat: created main nav & buy cake
    
    * feat: Working Buy cake section and added theme change
    
    * deleted unused files
    
    ---------
    
    Co-authored-by: María <m-i-y@live.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import ArrowButton from "./components/pages/home/arrowbutton/arrow-button.jsx";
+import './App.css'
+import ArrowButton from './components/pages/home/arrowbutton/arrow-button.jsx'
+import BuyCakeComponent from './components/shared/buycake/BuyCake.jsx';
+import SectionFooter from './components/shared/footer/SectionFooter.jsx'
 
 function App() {
-    return (
-        <>
-            <section
-                id="other-element-container"
-                className="other-element-container"
-            >
-                <ArrowButton />
-            </section>
-        </>
-    );
+  
+  
+  return (
+    < div>
+      
+      <section id="other-element-container" className="other-element-container">
+      <ArrowButton/>
+      </section >
+      <BuyCakeComponent/>
+      <SectionFooter />
+    </div >
+  )
 }
 
 export default App;
