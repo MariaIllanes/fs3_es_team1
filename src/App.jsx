1f22f5fe243ce84bdf59a890ddf198e52fd91c5d
dmg310802 <danielmtz@next2.com.mx>
2023-12-13 16:43:27 +0100

    feat: finished Main Banner component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ArrowButton from "./components/shared/arrow-button/ArrowButton.jsx";
 import BuyCakeComponent from "./components/shared/buycake/BuyCake.jsx";
 import SectionFooter from "./components/shared/footer/SectionFooter.jsx";
 import ExchangeStatsOne from "./components/pages/home/Exchange-stats-1/ExchangeStatsOne.jsx";
+import MainBannerComponent from "./components/pages/home/MainBanner/MainBanner";
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
             >
                 <ArrowButton />
             </section>
+            <MainBannerComponent />
             <BuyCakeComponent />
             <section id="sec4-container" className="sec4-container">
                 <ExchangeStatsOne />
--- src/components/shared/sharedComponents/ToggleSwitch.jsx
+++ src/components/shared/sharedComponents/ToggleSwitch.jsx
@@ -7,11 +7,11 @@ import '../sharedComponents/sharedStyles/modal.css';
 const ToggleSwitch = () => {
   return (
    <>
-    <label class="switch">
+    <label className="switch">
     <input type="checkbox" id="main"/>
-    <span class="slider round">
-        <div class="slider-images">
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon-ligth theme-change light-theme" id="icon-sun" width="25" height="25" viewBox="0 0 24 24" stroke-width="1.5" stroke="#27262c" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    <span className="slider round">
+        <div className="slider-images">
+            <svg xmlns="http://www.w3.org/2000/svg" className="icon-ligth theme-change light-theme" id="icon-sun" width="25" height="25" viewBox="0 0 24 24" stroke-width="1.5" stroke="#27262c" fill="none" stroke-linecap="round" stroke-linejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                 <path d="M14.828 14.828a4 4 0 1 0 -5.656 -5.656a4 4 0 0 0 5.656 5.656z" />
                 <path d="M6.343 17.657l-1.414 1.414" />
@@ -23,7 +23,7 @@ const ToggleSwitch = () => {
                 <path d="M20 12h2" />
                 <path d="M12 20v2" />
             </svg>
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon-light theme-change light-theme" id="icon-moon" width="25" height="25" viewBox="0 0 24 24" stroke-width="1.5" stroke="#9a6aff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <svg xmlns="http://www.w3.org/2000/svg" className="icon-light theme-change light-theme" id="icon-moon" width="25" height="25" viewBox="0 0 24 24" stroke-width="1.5" stroke="#9a6aff" fill="none" stroke-linecap="round" stroke-linejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill=""/>
                 <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" />
             </svg>
