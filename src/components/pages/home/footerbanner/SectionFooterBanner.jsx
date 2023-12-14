1925cdd65db9a7f2cb230c1e68d48435c8f12e43
María <m-i-y@live.com>
2023-12-14 10:42:58 +0100

    feat: sec11 working on hidden banner

--- src/components/pages/home/footerbanner/SectionFooterBanner.jsx
+++ src/components/pages/home/footerbanner/SectionFooterBanner.jsx
@@ -1,44 +1,40 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './FooterBannerStyle.css'
 import bfRock1 from "./FooterBannerAssets/bf-rock1.webp"
 import bfRock2 from "./FooterBannerAssets/bf-rock2.webp"
 import bfRock3 from "./FooterBannerAssets/bf-rock3.webp"
 import bfRock4 from "./FooterBannerAssets/bf-rock4.webp"
-import StandarButton from "../../../../components/pages/shared/StandardButton"
+// import StandarButton from "../../../../components/pages/shared/StandardButton"
 import ConnectWalletList from './ConnectWalletList';
 
 const SectionFooterBanner = () => {
-    const footerBannerButton = document.getElementById("button-s11");
-    const hiddenFooterBanner = document.querySelector(
-      "#footer-banner-hidden-section"
-    );
-    
-    let footerBannerisHidden = true;
-    
-    footerBannerButton.addEventListener("click", () => {
-      if (footerBannerisHidden) {
-        hiddenFooterBanner.style.display = "block";
-      } else {
-        hiddenFooterBanner.style.display = "none";
+  const [isHidden, setIsHidden] = useState(true);
+
+  const toggleHidden = () => {
+      setIsHidden(!isHidden);
+  };
+
+  const handleOutsideClick = (event) => {
+      const hiddenFooterBanner = document.querySelector("#footer-banner-hidden-section");
+      const footerBannerButton = document.getElementById("button-s11");
+
+      if (
+          !hiddenFooterBanner.contains(event.target) &&
+          event.target !== footerBannerButton &&
+          !isHidden
+      ) {
+          setIsHidden(true);
+          console.log("Clicked outside. footerBannerisHidden:", isHidden);
       }
-    
-      footerBannerisHidden = !footerBannerisHidden;
-      console.log(
-        "Show button clicked. footerBannerisHidden:",
-        footerBannerisHidden
-      );
-    });
-    
-    document.addEventListener("click", (event) => {
-      const isClickedOutside =
-        !hiddenFooterBanner.contains(event.target) &&
-        event.target !== footerBannerButton;
-      if (isClickedOutside && !footerBannerisHidden) {
-        hiddenFooterBanner.style.display = "none";
-        footerBannerisHidden = true;
-        console.log("Clicked outside. footerBannerisHidden:", footerBannerisHidden);
-      }
-    });
+  };
+
+  useEffect(() => {
+      document.addEventListener("click", handleOutsideClick);
+
+      return () => {
+          document.removeEventListener("click", handleOutsideClick);
+      };
+  }, [isHidden]);
     
     return (
         <div className='footer-banner theme-change light-theme'>
@@ -47,7 +43,7 @@ const SectionFooterBanner = () => {
                 <img src={bfRock2} width="120" height="120" alt="animation" class="footer-banner-img" id="footer-banner3" />
                 <div class="footer-banner-text">
                     <h2>Join Everyone's Favorite DEX Now!</h2>
-                    <StandardButton id="button-s11"  />
+                    <button id="button-s11"  />
                 </div>
                 <img src={bfRock3} width="120" height="94" alt="animation" class="footer-banner-img" id="footer-banner2" />
                 <img src={bfRock4} width="160" height="160" alt="animation" class="foote r-banner-img" id="footer-banner4" />
