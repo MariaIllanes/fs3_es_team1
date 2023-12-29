eb27f0dc39dd762161308a60bc04e5daa5f6374f
María Illanes <45496091+MariaIllanes@users.noreply.github.com>
2023-12-29 16:40:20 +0100

    S11/footerbanner almost ready (#11)
    * feat: s11 started
    
    * feat: s12 updated
    
    * feat: sec11 working on hidden banner
    
    * checking
    
    * feat: section 11 almost ready. left to do: dark + some css fixing
    
    * feat: completely functional. check responsive + s11-button. more info end of css
    
    * Update FooterBannerStyle.css to remove scroll bar
    
    * s11 almost ready
    
    * feat s11 still on development fase
    
    * feat: s11 cards open new section
    
    * s11 cards open new sections
    
    * feat: s11 cards active new content
    
    * feat: s11 just some dark mode to fix
    
    * doubled docs deleted
    
    * feat s11 missing some dark mode css
    
    * feat: s11 ready to merge + s8 syntax errors solved
    
    ---------
    
    Co-authored-by: Sarkix <142878605+Sarkix@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import SectionFooterBanner from "./components/pages/home/footerbanner/SectionFooterBanner"
 import SectionCommunity from "./components/pages/home/community/SectionCommunity.jsx";
 import ArrowButton from "./components/shared/arrow-button/ArrowButton.jsx";
 import BuyCakeComponent from "./components/shared/buycake/BuyCake.jsx";
@@ -8,6 +9,7 @@ import SectionExchangeStats from "./components/pages/home/exchangestats/SectionE
 import News from "./components/pages/home/News/News.jsx";
 import BottomNav from "./components/shared/bottom-nav/BottomNav.jsx";
 import MainNav from "./components/shared/main-nav/MainNav";
+
 
 function App() {
     return (
@@ -39,6 +41,7 @@ function App() {
             >
                 <News />
             </section>
+            <SectionFooterBanner />
             <SectionFooter />
         </div>
     );
