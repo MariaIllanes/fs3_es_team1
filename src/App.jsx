9c09b4bb619c2af78a1cbb0caeeb097e3d969609
María Illanes <45496091+MariaIllanes@users.noreply.github.com>
2023-12-29 17:02:07 +0100

    S5/ecosystem (#10)
    * feat: s11 started
    
    * feat: s12 updated
    
    * feat: sec11 working on hidden banner
    
    * feat s5 ready to merge
    
    * feat: s5 ready to merge. responsive fixed
    
    * feat: css elements alignment footer and main navbar
    
    * feat: sec checked. some css updated
    
    * git commit -m
    git commit -m
    
    * fixing errors on s5 and s9
    
    ---------
    
    Co-authored-by: Sarkix <142878605+Sarkix@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import "./App.css";
-import SectionFooterBanner from "./components/pages/home/footerbanner/SectionFooterBanner"
+import SectionFooterBanner from "./components/pages/home/footerbanner/SectionFooterBanner";
 import SectionCommunity from "./components/pages/home/community/SectionCommunity.jsx";
 import ArrowButton from "./components/shared/arrow-button/ArrowButton.jsx";
 import BuyCakeComponent from "./components/shared/buycake/BuyCake.jsx";
 import SectionFooter from "./components/shared/footer/SectionFooter.jsx";
 import ExchangeStatsOne from "./components/pages/home/Exchange-stats-1/ExchangeStatsOne.jsx";
+import SectionEcosystem from "./components/pages/home/ecosystem/SectionEcosystem.jsx";
 import SectionExchangeStats from "./components/pages/home/exchangestats/SectionExchangeStats.jsx";
 import News from "./components/pages/home/News/News.jsx";
 import BottomNav from "./components/shared/bottom-nav/BottomNav.jsx";
 import MainNav from "./components/shared/main-nav/MainNav";
-
 
 function App() {
     return (
@@ -32,6 +32,7 @@ function App() {
             <section id="sec4-container" className="sec4-container">
                 <ExchangeStatsOne />
             </section>
+            <SectionEcosystem />
             <SectionExchangeStats />
             <SectionCommunity />
             <section
--- src/components/pages/home/footerbanner/SectionFooterBanner.jsx
+++ src/components/pages/home/footerbanner/SectionFooterBanner.jsx
@@ -1,64 +1,110 @@
-import React, { useState, useRef, useEffect } from 'react';
-import './FooterBannerStyle.css';
+import React, { useState, useRef, useEffect } from "react";
+import "./FooterBannerStyle.css";
 import bfRock1 from "./FooterBannerAssets/bf-rock1.webp";
 import bfRock2 from "./FooterBannerAssets/bf-rock2.webp";
 import bfRock3 from "./FooterBannerAssets/bf-rock3.webp";
 import bfRock4 from "./FooterBannerAssets/bf-rock4.webp";
-import HiddenFooterBanner from './HiddenFooterBanner';
+import HiddenFooterBanner from "./HiddenFooterBanner";
 
 const SectionFooterBanner = () => {
-  const [isHidden, setIsHidden] = useState(true);
-  const containerRef = useRef(null);
-  const overlayRef = useRef(null);
+    const [isHidden, setIsHidden] = useState(true);
+    const containerRef = useRef(null);
+    const overlayRef = useRef(null);
 
-  const toggleHidden = () => {
-    setIsHidden(!isHidden);
-  };
-
-  const handleOverlayClick = () => {
-    setIsHidden(true); // Hides HiddenFooterBanner when overlay is clicked
-  };
-
-  const handleOutsideClick = (event) => {
-    if (
-      containerRef.current &&
-      overlayRef.current &&
-      !containerRef.current.contains(event.target) &&
-      event.target === overlayRef.current
-    ) {
-      handleOverlayClick();
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener('click', handleOutsideClick);
-
-    return () => {
-      document.removeEventListener('click', handleOutsideClick);
+    const toggleHidden = () => {
+        setIsHidden(!isHidden);
     };
-  }, [isHidden]);
-  
 
-  return (
-    <div className="footer-banner theme-change light-theme" ref={containerRef}>
-      <div id="footerbanner-visible-section">
-        <img src={bfRock1} width="120" height="120" alt="animation" className="footer-banner-img" id="footer-banner1" />
-        <img src={bfRock2} width="120" height="120" alt="animation" className="footer-banner-img" id="footer-banner3" />
-        <div className="footer-banner-text">
-          <h2>Join Everyone's Favorite DEX Now!</h2>
-          <button className='btn-main-blue' id="button-s11" onClick={toggleHidden}>Connect Wallet</button>
+    const handleOverlayClick = () => {
+        setIsHidden(true); // Hides HiddenFooterBanner when overlay is clicked
+    };
+
+    const handleOutsideClick = (event) => {
+        if (
+            containerRef.current &&
+            overlayRef.current &&
+            !containerRef.current.contains(event.target) &&
+            event.target === overlayRef.current
+        ) {
+            handleOverlayClick();
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener("click", handleOutsideClick);
+
+        return () => {
+            document.removeEventListener("click", handleOutsideClick);
+        };
+    }, [isHidden]);
+
+    return (
+        <div
+            className="footer-banner theme-change light-theme"
+            ref={containerRef}
+        >
+            <div id="footerbanner-visible-section">
+                <img
+                    src={bfRock1}
+                    width="120"
+                    height="120"
+                    alt="animation"
+                    className="footer-banner-img"
+                    id="footer-banner1"
+                />
+                <img
+                    src={bfRock2}
+                    width="120"
+                    height="120"
+                    alt="animation"
+                    className="footer-banner-img"
+                    id="footer-banner3"
+                />
+                <div className="footer-banner-text">
+                    <h2>Join Everyone's Favorite DEX Now!</h2>
+                    <button
+                        className="btn-main-blue"
+                        id="button-s11"
+                        onClick={toggleHidden}
+                    >
+                        Connect Wallet
+                    </button>
+                </div>
+                <img
+                    src={bfRock3}
+                    width="120"
+                    height="94"
+                    alt="animation"
+                    className="footer-banner-img"
+                    id="footer-banner2"
+                />
+                <img
+                    src={bfRock4}
+                    width="160"
+                    height="160"
+                    alt="animation"
+                    className="footer-banner-img"
+                    id="footer-banner4"
+                />
+            </div>
+            <div
+                className="footerbanner-hidden-section"
+                id="footer-banner-hidden-section"
+            >
+                <div
+                    className="theme-change light-theme"
+                    style={{ display: isHidden ? "none" : "block" }}
+                >
+                    <HiddenFooterBanner />
+                    <div
+                        className="overlay"
+                        ref={overlayRef}
+                        onClick={handleOverlayClick}
+                    ></div>
+                </div>
+            </div>
         </div>
-        <img src={bfRock3} width="120" height="94" alt="animation" className="footer-banner-img" id="footer-banner2" />
-        <img src={bfRock4} width="160" height="160" alt="animation" className="footer-banner-img" id="footer-banner4" />
-      </div>
-      <div className="footerbanner-hidden-section" id='footer-banner-hidden-section'>
-        <div className="theme-change light-theme" style={{ display: isHidden ? 'none' : 'block' }}>
-          <HiddenFooterBanner />
-          <div className="overlay" ref={overlayRef} onClick={handleOverlayClick}></div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default SectionFooterBanner;
