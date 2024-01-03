e43aeaf54ee1d830204c390f4e88909180d54b56
Sarkix <142878605+Sarkix@users.noreply.github.com>
2024-01-03 16:55:28 +0100

    Sec7/partners animation (#8)
    * Implement PartnersAnimation component in React.
    
    Created the PartnersAnimation.jsx component to transition the respective section from plain JavaScript to React. Also, added and imported the partners-animation.css file for styling.
    
    * style: Refine styles in partners-animation.css
    
    Made adjustments in partners-animation.css to enhance and fine-tune the styles for a better appearance.
    
    * style: Remove comments and apply ThemeChange
    
    Modified files to remove unnecessary comments and applied ThemeChange for consistency and improved styling.
    
    * style: Adjust styles for responsive carousel in PartnersAnimation
    
    Modified css and jsx files in the PartnersAnimation component to fine-tune styles, ensuring a responsive layout for the carousel without errors. Also fixed right-margin issues caused by ::after elements in the CSS.
    
    * style: Refine styles in PartnersAnimation.jsx
    
    Made style adjustments in the PartnersAnimation.jsx file to enhance the overall appearance of the component.
    
    * style: Adjust styles in modified files
    
    Made style adjustments in the modified files to enhance the overall appearance.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import ArrowButton from "./components/shared/arrow-button/ArrowButton.jsx";
 import BuyCakeComponent from "./components/shared/buycake/BuyCake.jsx";
 import SectionFooter from "./components/shared/footer/SectionFooter.jsx";
 import ExchangeStatsOne from "./components/pages/home/Exchange-stats-1/ExchangeStatsOne.jsx";
+import PartnersAnimation from "./components/pages/home/partners-animation/PartnersAnimation.jsx";
 import MainBannerComponent from "./components/pages/home/MainBanner/MainBanner";
 import SectionEcosystem from "./components/pages/home/ecosystem/SectionEcosystem.jsx";
 import SectionExchangeStats from "./components/pages/home/exchangestats/SectionExchangeStats.jsx";
@@ -17,7 +18,6 @@ function App() {
         <div>
             {/* <MainNav /> */}
             <section
-                style={{ boxSizing: "border-box" }}
                 id="reactive-nav-bottom"
                 className="reactive-nav-bottom light-theme theme-change"
             >
@@ -30,11 +30,19 @@ function App() {
                 <ArrowButton />
             </section>
             <MainBannerComponent />
-            <BuyCakeComponent />
             <section id="sec4-container" className="sec4-container">
                 <ExchangeStatsOne />
             </section>
             <SectionEcosystem />
+
+            <BuyCakeComponent />
+            <section
+                id="partners-animation"
+                className="first-container-partners theme-change light-theme"
+                style={{ zIndex: 5 }}
+            >
+                <PartnersAnimation />
+            </section>
             <SectionExchangeStats />
             <SectionCommunity />
             <section
